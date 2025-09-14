e512fea8644e6b221ebb50f88c2a04b72af3a382
root <sebastiaanbaskamphuis@gmail.com>
2025-09-14 17:14:08 +0200

    14-09-2025 (2)

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Voetbal site</title>
+</head>
+<body>
+    <header>
+        <div>
+            <h1>Voetbal site</h1>
+        </div>
+        <div>
+            <nav>
+                <a class="active" href="index.php">Home</a>
+                <a href="hetspel.php">Het Spel</a>
+                <a href="nieuws.php">Niews</a>
+                <a href="competities.php">Competite's</a>
+                <a href="eredivisie.php">Eredivisie</a>
+                <a href="contact.php">Contact</a>
+            </nav>
+        </div>        
+    </header>
+    <main>
+        <div>
+            <h2>Voetbal</h2>
+        </div>
+        <div>
+            <div>
+                <h3>Wat is voetbal?</h3>
+                <h4>in het kort</h4>
+            </div>
+            <div class="home-description">
+                <p>Voetbal is een teamsport waarbij twee teams van elf spelers een bal in het doel van de tegenstander proberen te krijgen. Het team dat aan het einde van de wedstrijd de meeste doelpunten heeft gemaakt, wint. Spelers gebruiken voornamelijk hun voeten om de bal te bewegen, vandaar de naam. meer weten? Ga naar tabblad <a href="hetspel.php">Het Spel</a></p>
+            </div>
+            <div>
+                <img class="home-veld" src="images/veld.jpg" alt="Voetbalveld">
+            </div>
+        </div>
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
